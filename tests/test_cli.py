7759d079708bf2d051c7cdd135c993ad97c06c19
AngusWG <740713651@qq.com>
2021-10-20 17:49:02 +0800

    fix: change encrypt_utils

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,16 +3,6 @@
 
 from fire import testutils
 
-from value_bank import __main__
-
 
 class CoreTest(testutils.BaseTestCase):
-    def test_version(self):
-        with self.assertOutputMatches(stdout=".*"):
-            __main__.fire.Fire(__main__.version, command=[])
-
-    def test_version_help_info(self):
-        with self.assertRaisesFireExit(0, regexp="显示当前版本"):
-            __main__.fire.Fire(
-                {"version": __main__.version}, command=["version", "--help"]
-            )
+    ...
--- value_bank/encrypt_utils.py
+++ value_bank/encrypt_utils.py
@@ -4,44 +4,46 @@
 # @author  : zza
 # @Email   : 740713651@qq.com
 # @File    : encrypt_utils.py
+
 import base64
 
-from Crypto.Cipher import AES
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 
-def add_to_16(value: str) -> bytes:
+def _format_key(value: str) -> bytes:
     """需要补位，str不是16的倍数那就补足为16的倍数"""
-    while len(value) % 16 != 0:
-        value += "\0"
-    return str.encode(value)  # 返回bytes
+    password = value.encode()
+    hkdf = HKDF(
+        algorithm=hashes.SHA256(),  # You can swap this out for hashes.MD5()
+        length=32,
+        salt=None,  # You may be able to remove this line but I'm unable to test
+        info=None,  # You may also be able to remove this line
+        backend=default_backend(),
+    )
+    key = base64.urlsafe_b64encode(hkdf.derive(password))
+    return key
 
 
 def encrypt(key: str, text: str) -> str:
     """加密方法"""
-    aes = AES.new(add_to_16(key), AES.MODE_ECB)  # 初始化加密器
-    encrypt_aes = aes.encrypt(add_to_16(text))  # 先进行aes加密
-    encrypted_text = str(
-        base64.encodebytes(encrypt_aes), encoding="utf-8"
-    )  # 执行加密并转码返回bytes
-    return encrypted_text
+    fer = Fernet(_format_key(key))
+    token = fer.encrypt(text.encode()).decode()
+    return token
 
 
 def decrypt(key: str, text: str) -> str:
     """解密方法"""
-    aes = AES.new(add_to_16(key), AES.MODE_ECB)  # 初始化加密器
-    base64_decrypted = base64.decodebytes(
-        text.encode(encoding="utf-8")
-    )  # 优先逆向解密base64成bytes
-    decrypted_text = str(aes.decrypt(base64_decrypted), encoding="utf-8").replace(
-        "\0", ""
-    )  # 执行解密密并转码返回str
-    return decrypted_text
+    fer = Fernet(_format_key(key))
+    return fer.decrypt(text.encode()).decode()
 
 
 if __name__ == "__main__":
-    key = "vbank"
-    text = "one python one day."
-    en_text = encrypt(key, text)
-    de_text = decrypt(key, en_text)
+    tmp_key = "v_bank"
+    content = "one python one day."
+    en_text = encrypt(tmp_key, content)
+    de_text = decrypt(tmp_key, en_text)
     print(en_text)
     print(de_text)
